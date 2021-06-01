cc0414f334cd44f267cdfe8b29fc1d1c03fcf015
DeepanshuRathore2000 <rathoredeepanshu2000@gmai.com>
2021-06-02 00:20:43 +0530

    sheet toggle done

--- grid.js
+++ grid.js
@@ -8,9 +8,9 @@ let underlineBtn = document.querySelector(".underline");
 let italicBtn = document.querySelector(".italic");
 let alignBtn = document.querySelectorAll(".align-container>*");
 let fontSizeElem = document.querySelector(".font-size");
-let formulaBar = document.querySelector(".formula-input");
 let rows = 100;
 let cols = 26;
+
 /*leftcol*/
 for(let i = 0; i < rows; i++){
     let colBox = document.createElement("div");
@@ -42,30 +42,89 @@ for (let i = 0; i < rows; i++) {
     }
     grid.appendChild(row);   
 }
-//database
-let sheetDB = [];
-for(let i=0;i<rows;i++){
-    let row = [];
-    for(let j=0;j<cols;j++){
-        let cell = {
-            bold: "normal",
-            italic:"normal",
-            underline:"none",
-            hAlign:"center",
-            fontFamily:"sans-serif",              // cells object ka data
-            fontSize:"16",
-            color:"black",
-            bgColor:"none",
-            value:"",
-            formula: "",
-            children: []
-        }
-        row.push(cell);                //row ke andar cells
+let btnContainer = document.querySelector(".add-btn-container");
+let sheetList = document.querySelector(".sheet-list")
+let firstSheet = document.querySelector(".sheet");
+let formulaBar = document.querySelector(".formula-input");
+let sheetArray = [];
+// current sheet
+let sheetDB;
+firstSheet.addEventListener("click",makeMeActive);
+
+firstSheet.click(); //click krwa diya first sheet pe
+btnContainer.addEventListener("click",function () {
+    //create sheet
+    let allSheets = document.querySelectorAll(".sheet");
+    let lastSheet = allSheets[allSheets.length-1];  //allsheet mein se last sheet nikalni
+    let lastIdx = lastSheet.getAttribute("idx");    // last index ki value nikaal li
+    lastIdx = Number(lastIdx);                      //index ko string to number me chang krdiya
+    let Newsheet = document.createElement("div");
+    Newsheet.setAttribute("class","sheet");
+    Newsheet.setAttribute("idx",`${lastIdx+1}`); // indx ka no.
+    Newsheet.innerText = `sheet ${lastIdx+2}`;     // sheet ka no. 
+    sheetList.appendChild(Newsheet);
+    for(let i=0;i<allSheets.length;i++){
+        allSheets[i].classList.remove("active");   // saare sheets se remove .active krdiya
     }
-    sheetDB.push(row);                //sheet ke andar rows 
-}                                       //2D array reprensents sheet and object represents cell
-
-
+    Newsheet.classList.add("active");              // new sheet pe remove krdiya  
+    //new sheet create
+    createSheet();
+    sheetDB = sheetArray[Lastidx+1];
+    Newsheet.addEventListener("click",handleSheet)
+})
+function makeMeActive(e) {
+    //evnt listener add
+    let sheet = e.currentTarget;                     // jis cheej pe click karoge wo e.currentTarget se milega
+    let allSheets = document.querySelectorAll(".sheet");   // saari sheet list is se mil jaayegi
+    for(let i=0;i<allSheets.length;i++){
+        allSheets[i].classList.remove("active");         // sab se ht jaayegi actibe class
+    }
+    sheet.classList.add("active");                      // jispe click kiya uspe active ho jaayegi
+    let idx = sheet.getAttribute("idx");
+    if(!sheetArray[idx]){
+        //only when you init the workbook
+        createSheet();
+    }
+    // when clicked on addsheet
+    sheetDB = sheetArray[idx];
+    setUI();
+}
+function createSheet(){
+    let NewDB=[];
+    for(let i=0;i<rows;i++){
+        let row = [];
+        for(let j=0;j<cols;j++){
+            let cell = {
+                bold: "normal",
+                italic:"normal",
+                underline:"none",
+                hAlign:"center",
+                fontFamily:"sans-serif",              // cells object ka data
+                fontSize:"16",
+                color:"black",
+                bgColor:"none",
+                value:"",
+                formula: "",
+                children: []
+            }
+            let elem = document.querySelector(`.grid .cell[rid='${i}'][cid='${j}']`);
+            elem.innerText = "";       // to clear UI
+            row.push(cell);                //row ke andar cells
+        }
+        NewDB.push(row);                //sheet ke andar rows 
+    }                                       //2D array reprensents sheet and object represents cell
+    sheetArray.push(NewDB);
+}
+function setUI() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            let elem = 
+            document.querySelector(`.grid .cell[rid='${i}'][cid='${j}']`);
+            let value = sheetDB[i][j].value;
+            elem.innerText = value;
+        }
+    }
+}        
 // we have to write this below because cell will be created later and we can take action on them afterwards only
 let allCells = document.querySelectorAll(".grid .cell");
 for(let i=0; i<allCells.length ;i++){
@@ -106,8 +165,22 @@ for(let i=0; i<allCells.length ;i++){
         }
     })
 }
-
-
+// *********formatting******//
+// horizontal alignment
+for(let i=0;i<alignBtn.length;i++){
+    alignBtn[i].addEventListener("click",function(){
+        let alignment = alignBtn[i].getAttribute("class");
+        let uiCellElement = findUICellElement();
+        uiCellElement.style.textAlign = alignment ;
+    })
+}
+//font-size
+fontSizeElem.addEventListener("change",function(){
+    let val = fontSizeElem.value;
+    let uiCellElement = findUICellElement();
+    uiCellElement.style.fontSize=val+"px";
+})
+allCells[0].click();
 // bold,underline,italic
 boldBtn.addEventListener("click", function (){
     // jispe click krein -> usey bold krdo
@@ -157,18 +230,3 @@ italicBtn.addEventListener("click", function (){
         cellObject.italic="normal";
     }
 })
-// horizontal alignment
-for(let i=0;i<alignBtn.length;i++){
-    alignBtn[i].addEventListener("click",function(){
-        let alignment = alignBtn[i].getAttribute("class");
-        let uiCellElement = findUICellElement();
-        uiCellElement.style.textAlign = alignment ;
-    })
-}
-//font-size
-fontSizeElem.addEventListener("change",function(){
-    let val = fontSizeElem.value;
-    let uiCellElement = findUICellElement();
-    uiCellElement.style.fontSize=val+"px";
-})
-allCells[0].click();
